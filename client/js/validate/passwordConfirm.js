7d534b79d63c42a5699acf778040d14cc405172d
Gabriel Veiga <gabriel.veiga_BOLS@compasso.com.br>
2019-06-13 18:05:47 -0300

    [FIX] remove client output js files. use the client build script to compile them when needed

--- client/js/validate/passwordConfirm.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function valPasswordConfirm(pw, confirm) {
-    return pw.value === confirm.value ? 'Senhas não batem' : null;
-}
-exports.valPasswordConfirm = valPasswordConfirm;
-//# sourceMappingURL=passwordConfirm.js.map
--- client/js/validate/passwordLength.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function valPasswordLength(pw) {
-    return (pw.value.length < 6 || pw.value.length > 8) ?
-        'Senha deve ter tamanho entre 6 e 8 dígitos.' : null;
-}
-exports.valPasswordLength = valPasswordLength;
-//# sourceMappingURL=passwordLength.js.map
