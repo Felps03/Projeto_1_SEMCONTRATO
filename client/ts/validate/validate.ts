2014846713a2c1bccbe708d8311a1b3177702446
Gabriel Veiga <gabriel.veiga_BOLS@compasso.com.br>
2019-06-13 17:51:14 -0300

    [FIX] fix confusing 'el' arg name

--- client/ts/validate/validate.ts
+++ client/ts/validate/validate.ts
@@ -1,18 +1,18 @@
 import { InputWrapper } from "./InputWrapper";
 
-export function validate(el: InputWrapper, fn: (el: InputWrapper, ...opts: any) => string, ...opts: any): (event: Event) => boolean {
+export function validate(input: InputWrapper, fn: (input: InputWrapper, ...opts: any) => string, ...opts: any): (event: Event) => boolean {
 
     return function (event: Event) {
-        const msg = fn(el, ...opts)
+        const msg = fn(input, ...opts)
 
         if (msg) {
-            el.setValid(false, msg)
+            input.setValid(false, msg)
             event.preventDefault()
 
             return false
         }
 
-        el.setValid(true)
+        input.setValid(true)
         return true
     }
 }
