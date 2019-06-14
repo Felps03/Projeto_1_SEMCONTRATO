72f6a5e8344c5365e3a660758fbd5ae378f7c675
Gabriel Veiga <gabriel.veiga_BOLS@compasso.com.br>
2019-06-14 16:56:26 -0300

    [FIX] fix userDao require

--- app/infra/userDao.js
+++ app/infra/userDao.js
@@ -1,18 +1,18 @@
-const UserSchema = require('../models');
+const UserSchema = require('../models/user');
 class UserDao {
 
     list(callback) {
         UserSchema.find({}).exec((err, docs) => {
-            if(err) return callback(err, null)
+            if (err) return callback(err, null)
             callback(null, docs);
         });
     }
 
     add(user, callback) {
         const { name, lastName, email, password, dateOfBirth } = user;
-     
+
         UserSchema.create({ name, lastName, email, password, dateOfBirth }, (err, docs) => {
-            if(err) return callback(err, null)
+            if (err) return callback(err, null)
             callback(null, docs);
         });
     }
@@ -20,18 +20,18 @@ class UserDao {
     update(user, id, callback) {
         const { name, lastName, email, password, dateOfBirth } = user;
         UserSchema.findByIdAndUpdate(id, { name, lastName, email, password, dateOfBirth }, { new: true }, (err, docs) => {
-            if(err) return callback(err, null)
+            if (err) return callback(err, null)
             callback(null, docs);
         });
     }
 
     remove(id, callback) {
         UserSchema.findByIdAndRemove(id, (err, docs) => {
-            if(err) return callback(err, null)
+            if (err) return callback(err, null)
             callback(null, docs);
         });
     }
-    
-} 
+
+}
 
 module.exports = UserDao;
