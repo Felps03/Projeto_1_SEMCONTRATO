27018d9205b9aafc36ad07dcae7ba19ccc52703b
julianasequeira <49956334+julianasequeira@users.noreply.github.com>
2019-06-14 15:38:16 -0300

    merge

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
-require('./app/controllers/index')(app);
-
 const port = process.env.PORT || 3000;
-=======
+
 require('./app/controllers/authController');
->>>>>>> c6720247ee22c188df5802aea0a862c0539b61f1
 
 app.listen(port);
