75857a3d6516c1a85c08ad058707429d0719561c
Marcos Goulart de Oliveira <marcos.oliveira_BOLS@pampa.compasso>
2019-06-17 17:12:28 -0300

    [FIX] add cor

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+cors = require('cors');
+
+var corsOption = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+};
 
 const app = express();
+
+app.use(cors(corsOption));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
