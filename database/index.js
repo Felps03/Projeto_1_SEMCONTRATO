316e19fd4ae346872c717712244280e9ad8054d6
Marcos Paulo Vaz Oleiro <marcoS.oleiro_BOLS@pampa.compasso>
2019-06-18 16:57:55 -0300

    [FIX] Changing URL to connect with remote database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/semcontrato', { useMongoClient: true });
+mongoose.connect('mongodb+srv://semcontrato:C0nnect123@semcontrato-wslsh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
