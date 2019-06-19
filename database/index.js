83edf6843435085c121442f41812c5023dafae14
Felps03 <felipe.031293@gmail.com>
2019-06-18 22:39:40 -0300

    moogose

--- database/index.js
+++ database/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 
-
-// mongoose.connect('mongodb+srv://semcontrato:C0nnect123@semcontrato-wslsh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
-
-mongoose.connect('mongodb+srv://deploy:semcontrato@cluster0-xmagn.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://deploy:semcontrato@cluster0-xmagn.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+})
+.then(() => console.log('mongoose ok...'))
+.catch(err =>console.log(err));
 
 
 
