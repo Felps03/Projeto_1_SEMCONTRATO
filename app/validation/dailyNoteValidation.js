68f47c5664e9bbfb6df5273c82bca57ad22da8f0
Marcos Goulart de Oliveira <marcos.oliveira_BOLS@pampa.compasso>
2019-06-24 17:24:37 -0300

    [FIX] updating id_user validation in DailyNote

--- app/validation/dailyNoteValidation.js
+++ app/validation/dailyNoteValidation.js
@@ -3,14 +3,18 @@ const { check } = require('express-validator/check');
 class DailyNote {
     static validation() {
         return [
-            check('id_user').withMessage('O id do Usuario deve ser existente'),
-            check('yesterday').isLength({ min: 3 }).withMessage('O campo Ontem precisa ter no mínimo 3 caracteres!'),
-            check('today').isLength({ min: 3 }).withMessage('O campo Hoje precisa ter no mínimo 3 caracteres!'),
-            check('impediment').isLength({min: 3}).withMessage('O campo Impedimento precisa ter no minimo 3 caracteres!'),
-            check('date').custom(this.isValidDate).withMessage('A data é inválida. Deve estar no formato { aaaa-mm-dd }')
+            check('id_user').custom(this.isNull).withMessage('O id do USUARIO deve ser existente'),
+            check('yesterday').isLength({ min: 3 }).withMessage('O campo ONTEM precisa ter no mínimo 3 caracteres!'),
+            check('today').isLength({ min: 3 }).withMessage('O campo HOJE precisa ter no mínimo 3 caracteres!'),
+            check('impediment').isLength({min: 3}).withMessage('O campo IMPEDIMENTO precisa ter no minimo 3 caracteres!'),
+            check('date').custom(this.isValidDate).withMessage('A DATA é inválida. Deve estar no formato { aaaa-mm-dd }')
         ]
     }
 
+    static isNull(value){
+        if(!value) return false;
+        return value;
+    }
     static isValidDate(value) {
         if (!value.match(/^\d{4}-\d{2}-\d{2}$/)) return false;
 
