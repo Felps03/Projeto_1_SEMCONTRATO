f55c8c8a634fad37d392b6e2e4eef0e834166934
Felipe da Silva dos Santos <felipe.santos_BOLS@pampa.compasso>
2019-06-26 15:04:46 -0300

    error json

--- app/controllers/dailyNoteController.js
+++ app/controllers/dailyNoteController.js
@@ -1,4 +1,3 @@
-
 const { validationResult } = require('express-validator/check');
 const { Controller } = require('./Controller');
 
@@ -8,12 +7,12 @@ const UserDao = require('../infra/userDao');
 class DailyNoteController extends Controller {
     static rotas() {
         return {
-            cadastro: '/dailys/daily/', 
+            cadastro: '/dailys/daily/',
             edicao: '/dailys/daily/:id',
-            lista: '/dailys/daily/', 
+            lista: '/dailys/daily/',
         }
     }
-    
+
     add() {
         return (req, resp) => {
             const error = validationResult(req);
@@ -24,31 +23,25 @@ class DailyNoteController extends Controller {
             }
 
             let userDao = new UserDao();
-            userDao.findById(req.body.id_user, (error, result) => {
-                if(!result) {
-                    return resp.status(400).send('USUARIO não existente');
-                }
-            });
             const dailyNoteDao = new DailyNoteDao();
 
-            dailyNoteDao.findByUserDate(req.body.id_user, req.body.date, (error, result)=>{
-                if(result) {
-                    return resp.status(400).send("DAILY já cadastrada hoje!");
+            userDao.findById(req.body.id_user, (error, resultByID) => {
+                if (!resultByID) {
+                    return resp.status(400).send('USUARIO não existente');
                 }
+
+                dailyNoteDao.findByUserDate(req.body.id_user, req.body.date, (error, resultUserDate) => {
+                    if (resultUserDate) return resp.status(400).send(JSON.stringify({ erro: "DAILY já cadastrada hoje!" }));
+
+
+
+                    dailyNoteDao.add(req.body, (error, resultADD) => {
+                        if (resultADD) return resp.status(400).send('Houve Algum problema na hora de cadastrar o usuario favor olhar o log');
+
+                        resp.send(result);
+                    });
+                });
             });
-
-            dailyNoteDao.add(req.body, (error, result) => {
-                if (error) {
-                    console.log(error);
-                    resp.status(400).send('Houve Algum problema na hora de cadastrar o usuario favor olhar o log');
-                }
-
-                let response = {
-                    result
-                }
-
-                resp.send(response);
-            })    
         };
     }
 
@@ -73,7 +66,7 @@ class DailyNoteController extends Controller {
             });
         }
     }
-   
+
     list() {
         return (req, resp) => {
 
@@ -88,7 +81,7 @@ class DailyNoteController extends Controller {
             });
         }
     }
-   
+
     remove() {
         throw new Error('O método deletar não existe');
     }
