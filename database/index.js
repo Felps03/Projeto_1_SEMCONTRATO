bada3e1733f8da0ef6006851c1235e506786ee4d
Felipe da Silva dos Santos <felipe.santos_BOLS@pampa.compasso>
2019-06-28 14:52:25 -0300

    database

---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const MONGO_DEV = 'mongodb+srv://deploy:semcontrato@cluster0-xmagn.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_LOCAL = 'mongodb://localhost/noderest';
+
+mongoose.connect(MONGO_DEV, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('mongoose ok...'))
+    .catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+// mongoose.set('useFindAndModify', false);
+
+module.exports = mongoose;
