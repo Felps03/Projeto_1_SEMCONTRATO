71c502f2521e592ae4ddcb4aeea864d857c9e340
Marcos Paulo Vaz Oleiro <marcoS.oleiro_BOLS@pampa.compasso>
2019-07-04 15:00:58 -0300

    [FIX] Changing Database to dev. DB
    modified:   database/index.js

--- database/index.js
+++ database/index.js
@@ -4,8 +4,8 @@ const MONGO_DEV = 'mongodb+srv://deploy:semcontrato@cluster0-xmagn.mongodb.net/t
 // const MONGO_LOCAL = 'mongodb://localhost/noderest';
 const MONGO_LOCAL = 'mongodb://localhost/teste';
 
-// mongoose.connect(MONGO_DEV, {
-mongoose.connect(MONGO_LOCAL, {
+mongoose.connect(MONGO_DEV, {
+    // mongoose.connect(MONGO_LOCAL, {
     useNewUrlParser: true
 })
     // .then(() => console.log('mongoose ok...'))
