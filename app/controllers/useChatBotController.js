c3a8027b9a1a231f155cd3c849eb2df60004b79f
Marcos Paulo Vaz Oleiro <marcoS.oleiro_BOLS@pampa.compasso>
2019-07-30 15:01:59 -0300

    [FIX] Changin const UseChatBotDao = require('../infra/UseChatBotDao') to const UseChatBotDao = require('../infra/useChatBotDao')
    on useChatBotController.js
    	modified:   app/controllers/useChatBotController.js

--- app/controllers/useChatBotController.js
+++ app/controllers/useChatBotController.js
@@ -1,4 +1,4 @@
-const UseChatBotDao = require('../infra/UseChatBotDao')
+const UseChatBotDao = require('../infra/useChatBotDao')
 const getTokenFromHeader = require('../utils/getTokenFromHeader')
 
 class UseChatBotController {
@@ -34,7 +34,7 @@ class UseChatBotController {
 
             const useChatBotDao = new UseChatBotDao();
 
-            useChatBotDao.listByAccess(req.params.id_access ,(err, result) => {
+            useChatBotDao.listByAccess(req.params.id_access, (err, result) => {
                 if (err) {
                     return res.send(err)
                 }
@@ -49,10 +49,10 @@ class UseChatBotController {
 
             useChatBotDao.add(req.body.id_access,
                 (err, result) => {
-                    if(err){
+                    if (err) {
                         return res.status(400).send(
                             JSON.stringify(
-                                {erro: 'Erro na contagem de uso do CHATBOT.'}
+                                { erro: 'Erro na contagem de uso do CHATBOT.' }
                             )
                         );
                     }
@@ -67,11 +67,11 @@ class UseChatBotController {
             const useChatBotDao = new UseChatBotDao();
 
             useChatBotDao.update(req.body, req.params.id,
-                (err, result) =>{
-                    if(err){
+                (err, result) => {
+                    if (err) {
                         return res.status(400).send(
                             JSON.stringify(
-                                {erro: 'Erro no upload de registro de acesso á página.'}
+                                { erro: 'Erro no upload de registro de acesso á página.' }
                             )
                         );
                     }
@@ -88,10 +88,10 @@ class UseChatBotController {
 
             useChatBotDao.remove(req.params.id,
                 (err, result) => {
-                    if(err){
+                    if (err) {
                         return res.status(400).send(
                             JSON.stringify(
-                                {erro: 'Erro ao remover redistro de acesso á página.'}
+                                { erro: 'Erro ao remover redistro de acesso á página.' }
                             )
                         )
                     }
